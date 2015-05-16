1719717a07fda82744bde274e0a60308a9962179
Matthew <matthew@synthesis.co.za>
2015-05-16 17:37:26 +0200

    more rewrite code.

---
+++ BasicbotF#/BotBasic.fs
@@ -0,0 +1,52 @@
+﻿namespace Matt.SpaceInvaders
+
+module BotBasic =
+
+    open System.Diagnostics
+    open System
+    open System.IO
+
+    type ShipCommand =
+        Nothing = 0 
+        | MoveLeft = 1
+        | MoveRight = 2 
+        | Shoot = 3 
+        | BuildAlienFactory = 4 
+        | BuildMissileController = 5 
+        | BuildSheild = 6
+
+    let Log message =
+        printfn "[BOT]\t%s" message
+
+    let SaveShipCommand shipCommand =
+        let fileName = Path.Combine("", "")
+
+        try
+            use file = new StreamWriter(fileName)
+            file.WriteLine (shipCommand.ToString())
+            printfn "Ship Command: %s" shipCommand
+        with
+        | _ as e -> 
+            printfn "Unable to wtrite to command file: %s" e.Message
+            let trace = new StackTrace(e)
+            let traceStr = trace.ToString()
+            printfn "Stacktrace: %s" traceStr
+
+    let GetRandomShipCommand =
+        let random = new Random()
+        let possibleShipCommands = Enum.GetValues(typeof<ShipCommand>)
+        let choice =  (possibleShipCommands.GetValue(random.Next(0,possibleShipCommands.Length)).ToString())
+        Enum.GetName(typeof<ShipCommand>, choice)
+
+    let LoadMap =
+        let fileName = Path.Combine("", "")
+        try
+            use file = new StreamReader(fileName)
+            file.ReadToEnd()
+        with
+        | _ as e -> 
+            printfn "Unable to wtrite to command file: %s" e.Message
+            let trace = new StackTrace(e)
+            let traceStr = trace.ToString()
+            printfn "Stacktrace: %s" traceStr
+            "Failed to load map!"
--- BasicbotF#/Program.fs
+++ BasicbotF#/Program.fs
@@ -1,7 +1,36 @@
-﻿// Learn more about F# at http://fsharp.net
-// See the 'F# Tutorial' project for more help.
+﻿open System.IO
+open System
+
+
+let printUsage =
+    printfn "C# BasicBot usage: BasicBot.exe <outputFilename>"
+    printfn ""
+    printfn "\toutputPath\tThe output folder where the match runner will output map and state files and look for the move file."
+
+
+let runBot (args:string[]) =
+    
+    let mutable result = None
+
+    if args.Length <> 1 then
+        printUsage
+        Environment.Exit(1)
+        result
+    
+    let outputPath = args.GetValue 0
+    if Directory.Exists outputPath then
+        printUsage
+        printfn ""
+        printfn "Error: Output folder \%s\" does not exist." outputPath
+    result <- (Some "")
+    result
+    
+    
+
 
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    let result = runBot argv
     0 // return an integer exit code
+
